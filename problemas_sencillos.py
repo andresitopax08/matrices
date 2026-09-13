b593002ec1a9e3bb3c04461ecc8fe25680dfa492
Andres Clavijo <ylaeccompany@gmail.com>
2026-09-13 11:54:19 -0500

    Correciones del Codigo

--- problemas_sencillos.py
+++ problemas_sencillos.py
@@ -2,10 +2,13 @@ def calcularTotal(precio, cantidad):
     total = precio * cantidad
     return total
 
-precio_unitario = float(input("Ingrese el precio unitario del producto: "))
-cantidad_producto = int(input("Ingrese la cantidad de productos: "))
-total_a_pagar = calcularTotal(precio_unitario, cantidad_producto)
+if __name__ == "__main__":
+
+    precio_unitario = float(input("Ingrese el precio unitario del producto: "))
+    cantidad_producto = int(input("Ingrese la cantidad de productos: "))
+    total_a_pagar = calcularTotal(precio_unitario, cantidad_producto)
 
 print(f"El precio unitario es: {precio_unitario}")
 print(f"El total a pagar es: {total_a_pagar}")
 print("Gracias por su compra.")
+
